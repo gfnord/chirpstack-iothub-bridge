c669f937ac1eca717d78af1fc1318176f30f95e7
Gustavo Nobrega <gustavofn@gmail.com>
2020-03-30 19:09:51 -0700

    Replaced print by logger.info

--- bridge_chirpstack_iothub.py
+++ bridge_chirpstack_iothub.py
@@ -23,9 +23,7 @@ broker_password = 'bridgepassword'
 application_id = '1'  # change for your application id in chirpstack
 
 
-#####
 # IOT HUB SDK
-#####
 def iothub_client_init():
     # Create an IoT Hub client
     return IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
@@ -52,26 +50,26 @@ def on_message(client, userdata, message):
         logging.warning('Parsed JSON did not contain "object".')
 
     else:
-        print('DeviceID : ', json_data.get('devEUI'))
+        logger.info('DeviceID : ', json_data.get('devEUI'))
         obj['application_id'] = application_id
         obj['devEUI'] = json_data['devEUI']
 
         json_data_str = json.dumps(obj)
 
-        print('Object: ', json_data_str)
-        print('Azure: sending message...')
+        logger.info(f'Object: {json_data_str}')
+        logger.info('Azure: sending message...')
 
         message_to_azure = Message(json_data_str)
         message_to_azure.message_id = uuid.uuid4()
         message_to_azure.correlation_id = 'correlation-1234'
 
-        print('Azure:Connecting azure client')
+        logger.info('Azure:Connecting azure client')
         azure_client.connect()
 
-        print('Azure:Connected Azure client. Sending message...')
+        logger.info('Azure:Connected Azure client. Sending message...')
         azure_client.send_message(message_to_azure)
 
-        print('Azure: Message Sent. ')
+        logger.info('Azure: Message Sent. ')
         azure_client.disconnect()
 
 
@@ -79,11 +77,11 @@ def on_message(client, userdata, message):
 client = mqtt.Client('bridge')  # create new instance
 client.on_message = on_message  # attach function to callback
 
-print('Connecting to pub/sub broker.')
+logger.info('Connecting to pub/sub broker.')
 client.username_pw_set(username=broker_user, password=broker_password)
 client.connect(broker_address, broker_port)  # connect to broker
 
-print('Mosquitto: Subscribing to device rx topic')
+logger.info('Broker: Subscribing to device rx topic')
 client.subscribe('application/' + application_id + '/device/+/rx')
 
 client.loop_forever()
